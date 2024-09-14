1335e7d5bf6bf1ee71c26850ff5e0815de6371cf
Theecanmole <theecanmole@gmail.com>
2024-09-14 18:51:40 +1200

    update

--- rscript.r
+++ rscript.r
@@ -30,6 +30,7 @@ table(ghgs[["parameter"]])
                   1319                   1319                   1319 
                  trend 
                   1319
+
 # select only methane records
 methane <- ghgs[ghgs[["variable"]]=="methane",]
 str(methane) 
@@ -107,48 +108,9 @@ theme(legend.position = c(0.75,0.82),legend.title = element_text(NULL), plot.cap
 labs(title="Baring Head methane concentrations 1989 to 2023", caption="Data: Stats NZ Tatauranga Aotearoa \nhttps://www.stats.govt.nz/indicators/greenhouse-gas-concentrations") +   ylab("parts per billion") +  xlab("Year") 
 dev.off() 
 
-# change period end date column from character to date format 
-state_data[["period_end"]] <- as.Date(state_data[["period_end"]]) 
-
-str(state_data) 
-'data.frame':	1443 obs. of  5 variables:
- $ period_start      : chr  "1989-08-01" "1989-09-01" "1989-10-01" "1989-11-01" ...
- $ period_end        : Date, format: "1989-08-31" "1989-09-30" ...
- $ concentration     : num  1659 1662 1668 1669 1664 ...
- $ gas               : chr  "Methane" "Methane" "Methane" "Methane" ...
- $ reference_standard: chr  "WMOX2004a" "WMOX2004a" "WMOX2004a" "WMOX2004a" ... 
-
-table(state_data[["gas"]]) 
- Carbon dioxide Carbon monoxide         Methane   Nitrous oxide 
-            565             232             365             281  
-# select only methane records
-methane <- state_data[state_data[["gas"]]=="Methane",]
-# or via dplyr
-methane <- filter(state_data, gas =="Methane")
-
-str(methane)
-'data.frame':	365 obs. of  5 variables:
- $ period_start      : chr  "1989-08-01" "1989-09-01" "1989-10-01" "1989-11-01" ...
- $ period_end        : Date, format: "1989-08-31" "1989-09-30" ...
- $ concentration     : num  1659 1662 1668 1669 1664 ...
- $ gas               : chr  "Methane" "Methane" "Methane" "Methane" ...
- $ reference_standard: chr  "WMOX2004a" "WMOX2004a" "WMOX2004a" "WMOX2004a" .. 
- 
-svg(filename="NZmethane-2019-720by540.svg", width = 8, height = 6, pointsize = 14, onefile = FALSE, family = "sans", bg = "white", antialias = c("default", "none", "gray", "subpixel"))
-#png("NZmethane-2019-560by420.png", bg="white", width=560, height=420,pointsize = 14)
-# ,ylim=c(0,33), xlim=c(1989,2017)
-#jpeg(filename = "NZmethane-2021-560by420.jpeg", width = 640, height = 480, units = "px", pointsize = 16, quality = 100)
-#jpeg(filename = "NZmethane-2021-640by640.jpeg", width = 640, height = 640, units = "px", pointsize = 16, quality = 100)
-par(mar=c(2.7,3.3,1,1)+0.1)
-plot(methane[["period_end"]],methane[["concentration"]],tck=0.01,ylab=NA,axes=T,ann=T, type="l",lwd=2,las=1,col="#7570B3")
-box(lwd=1)
-mtext(side=1,line=-1.4,cex=1,"Data: Stats NZ Tatauranga Aotearoa \nhttps://www.stats.govt.nz/indicators/greenhouse-gas-concentrations")
-mtext(side=3,cex=1.3, line=-1.8,expression(paste("Baring Head Methane concentrations 1989 to 2019")) )
-mtext(side=2,cex=1.1, line=-1.5,expression(paste("Parts per billion")))
-mtext(side=4,cex=0.75, line=0.05,R.version.string)
-dev.off()
             
-# select only Nitrous oxide records
+## select only Nitrous oxide records
+
 nitrousoxide <- ghgs[ghgs[["variable"]]=="nitrous_oxide",]
 
 str(nitrousoxide) 
@@ -240,5 +202,3 @@ theme(legend.position = c(0.75,0.82),legend.title = element_text(NULL), plot.cap
 labs(title="Baring Head carbon dioxide concentrations 1972 to 2023", caption="Data: Stats NZ Tatauranga Aotearoa \nhttps://www.stats.govt.nz/indicators/greenhouse-gas-concentrations") +   ylab("parts per million") +  xlab("Year") +
 theme(legend.title=element_blank()) 
 dev.off() 
-
-getwd()
